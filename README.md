72abad7ee0fc6f58a117b84f1e24dafedcc39b09
Gary Su <gengwensu@yahoo.com>
2018-01-27 16:10:04 -0500

    some code

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Code Language:
 Please provide this using Javascript ES 5.0 not ES6 code. You can use anything in default javascript and any chrome extension calls as well.
 
 This code should be able to work in the majority of cases if we copy and pasted it onto any site as a javascript snippet
+
+# Usage
+
+callWhenReadyToGo(callback) should be used as a callback function of observeDOM, a cross-browser snippet that sets up a mutationObserver to observe DOM changes.(see https://stackoverflow.com/questions/3219758/detect-changes-in-the-dom/14570614#14570614)  For example: 
+
+
+```
+observeDOM(document.body, callWhenReadyToGo(alert('All images loaded!')));
+```
+
+It will go through all <img> tags in the dom.body and monitor whether there're changes in these tags. It will wait untill all the images are loaded then fires the callback.
--- callWhenReadyToGo.js
+++ callWhenReadyToGo.js
@@ -1,7 +1,9 @@
 /*
   callWhenReadyToGo : This snippet observes DOM for adding new medias (images or videos that take some time to load). When all medias are loaded, callback will be fired.
   This code should be able to work in the majority of cases if we copy and pasted it onto any site as a javascript snippet
- */
+ 
+callWhenReadyToGo(callback) will go through all the img tages in the dom body to see if any is loading currently. It'll wait till all imag tags are loaded and fire callback.
+  */
 var observeDOM = (function () {
     var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
         eventListenerSupported = window.addEventListener;
@@ -13,7 +15,7 @@ var observeDOM = (function () {
                 if (mutations[0].addedNodes.length || mutations[0].removedNodes.length)
                     callback();
             });
-            // have the observer observe foo for changes in children
+            // have the observer observe for changes in children
             obs.observe(obj, { childList: true, subtree: true });
         }
         else if (eventListenerSupported) {
@@ -23,44 +25,35 @@ var observeDOM = (function () {
     };
 })();
 
-// Observe a specific DOM element:
-observeDOM(document.getElementById('dom_element'), function () {
-    console.log('dom changed');
-});
+// Observe the DOM body for changes
+observeDOM(document.body, callWhenReadyToGo(alert('All images loaded!')));
 
-// Observe the DOM for changes
-observeDOM(document.body, function () {
-    checkNewMedia();
-});
-
-// Loop through all new media, add the event
-var checkNewMedia = function () {
-
-    // extend this by using document.querySelectorAll("img:not([data-loaded), video:not([data-loaded])") etc.
+// Loop through all new images, add the event
+var callWhenReadyToGo = function (callback) {
+    // check images loading only, change type if needed, data-loaded is just a flag to prevent multiple checks
     var media = document.querySelectorAll('img:not([data-loaded]');
     for (var i = 0; i < media.length; i++) {
-        addMediaLoadedEvent(media[i]);
+        addMediaLoadedEvent(media[i], callback);
     }
 }
 
 // Fire the callback if complete, otherwise bind to onload
-var addMediaLoadedEvent = function (element) {
+var addMediaLoadedEvent = function (element, callback) {
     if (element.complete) {
-        onMediaLoaded(element);
+        onMediaLoaded(element, callback);
     } else {
         element.addEventListener('load', function (event) {
-            onMediaLoaded(event.target);
+            onMediaLoaded(event.target, callback);
         });
     }
 }
 
 // The callback that is fired once an element is loaded
-var onMediaLoaded = function (element) {
+var onMediaLoaded = function (element, callback) {
     element.setAttribute('data-loaded', 'true');
 
     // only fire when there are no media elements without the 'data-loaded' attribute left
-    // again, can be extended to use video, etc.
     if (document.querySelectorAll('img:not([data-loaded])').length === 0) {
-        adjustHeights(); // <-- your function
+        callback(); // callback function fired
     }
 }
